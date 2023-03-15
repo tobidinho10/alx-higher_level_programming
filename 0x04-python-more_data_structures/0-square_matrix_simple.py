ad4393c87aed0c52ad96e19e24342ecc8f846cde
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2023-03-15 04:10:38 -0700

    0x4 pyhton

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
+r/bin/python3
+def square_matrix_simple(matrix=[]):
+        new_matrix = matrix.copy()
+
+            for i in range(len(matrix)):
+                        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+                            return (new_matrix)
+                        
